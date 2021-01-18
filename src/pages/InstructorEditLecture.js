8eec47195ed4da6ccc591b12dd289528f8b6e4f3
unknown <restlessankur@gmail.com>
2021-01-19 02:28:05 +0530

    add lecture feature done

--- src/pages/InstructorEditLecture.js
+++ src/pages/InstructorEditLecture.js
@@ -1,10 +1,11 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
-import { Divider, Grid, Typography } from '@material-ui/core';
+import { Divider, Grid, Typography, Button } from '@material-ui/core';
 import { gql, useQuery } from '@apollo/client';
 
 
+ import AddNewLectures from '../components/ui/AddNewLectures';
 const getLectures = gql`
 
 query MyQuery {
@@ -28,83 +29,84 @@ export default function InstructorEditLecture(props) {
 
     const [items, setItems] = useState([]);
     const { loading, error, data } = useQuery(getLectures);
-  
-    console.log("use effect",data)
+   
+
+    console.log("use effect", data)
     const SortableItem = SortableElement(({ value }) => <li >{value}</li>)
 
-const SortableList = SortableContainer(({ items }) => {
-    console.log("items in sortable container",items)
-    return (
-        <ul >
-            {
-                items.map((value, index) => (
-                    <div  style={{  height: "60px" }}>
-                        <SortableItem key={`item-${value.snumber}`} index={index} value={value.title} />
-                        <Divider />
 
-                    </div>
+   
+    const SortableList = SortableContainer(({ items }) => {
+        console.log("items in sortable container", items)
+        return (
+            <ul >
+                {
+                    items.map((value, index) => (
+                        <div style={{ height: "60px" }}>
+                            <SortableItem key={`item-${value.snumber}`} index={index} value={value.title} />
+                            <Divider />
 
-                ))
-            }
+                        </div>
 
-        </ul>
+                    ))
+                }
 
-    )
-})
-useEffect(() => {
-    // check if data exists or data isn't empty
-    if(data) {
-        setItems(data.lectures);
-    }
+            </ul>
 
-}, [data]);
-    if (loading){
+        )
+    })
+    useEffect(() => {
+        
+        if (data) {
+            setItems(data.lectures);
+        }
+
+    }, [data]);
+    if (loading) {
         console.log("loading")
         return 'Loading...';
-    } 
+    }
     if (error) return `Error! ${error.message}`;
-    
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        console.log("on sort end",oldIndex,newIndex)
-         setItems(arrayMove(items, oldIndex, newIndex));
-        
-    };
-   
 
-    console.log("loading is",loading)
-    console.log("error is",error)
-    console.log("data is",data)
-    console.log("items now",items)
-   
-        
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        console.log("on sort end", oldIndex, newIndex)
+        setItems(arrayMove(items, oldIndex, newIndex));
+
+    };
+    const lectureButtonClicked = () => {
+        console.log("lecture button clicked")
+    }
+
+
+    console.log("loading is", loading)
+    console.log("error is", error)
+    console.log("data is", data)
+    console.log("items now", items)
+
+
     return (
         <>
-         {loading && (<h1> loading...</h1>)}
-         { data ? (<Grid style={{ marginTop: "80px" }} container direction="row">
-            <Grid item sm={4}>
-                <SortableList items={items } onSortEnd={onSortEnd} />
-
-            </Grid>
-            <Grid item>
-                <h1>cool</h1>
+            {loading && (<h1> loading...</h1>)}
+            { data ? (<Grid style={{ marginTop: "80px" }} container direction="row">
+                <Grid item sm={4}>
+                    <SortableList items={items} onSortEnd={onSortEnd} />
+                    <AddNewLectures/>
+                </Grid>
+                <Grid item sm={8}>
                 
-                <Typography variant="h3"> this is great</Typography>
-                <Typography variant="h3"> this is great</Typography>
+                </Grid>
+                <Grid item>
+                    
+                </Grid>
+                
             </Grid>
-        </Grid>): null}
-               
+            
+            ) : null}
+            
+
 
 
         </>
-        
-
-
-
-
     )
-
-    
-
-
 
 }
